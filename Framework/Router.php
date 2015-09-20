526a295643fbd94185ed47e1e048cd18c01084c7
Kacper Czochara <kacperczochara@gmail.com>
2015-09-20 15:17:14 +0200

    phpDoc for Router and Rule

--- Framework/Router.php
+++ Framework/Router.php
@@ -2,16 +2,25 @@
 
 namespace Framework;
 
+use Framework\Exception\HttpException;
+use Framework\Exception\InvalidConfigException;
+
 /**
  * Class Router matches the request route with [[Rule]].
- * When matched [[Request]] object is being filled with parameters from route
+ * When matched [[Request]] object is being filled with parameters from route.
  *
  * @package Framework
  */
 class Router
 {
+    /**
+     * @var array of [[Rule]] objects
+     */
     private $_rules = [];
 
+    /**
+     * @param null $config Configuration array
+     */
     public function __construct($config = null)
     {
         if (isset($config['rules'])) {
@@ -21,11 +30,29 @@ class Router
         }
     }
 
+    /**
+     * Creates new [[Rule]] object and stores it in an array.
+     *
+     * @param array $rule Rule configuration array
+     */
     public function addRule($rule)
     {
         $this->_rules[] = new Rule($rule['pattern'], $rule['route']);
     }
 
+    /**
+     * Resolves the request.
+     * Matches the request uri with the set of rules.
+     * When the rule is found, then [[HttpRequest]] object is filled with parameters values coming from the request uri.
+     * Otherwise an [[HttpException]] is thrown.
+     *
+     * It also figures out what the controller and action are.
+     *
+     * @param HttpRequest $request The incoming request
+     * @return HttpRequest $request Filled with parameters from request uri
+     * @throws HttpException When no rule is found and request cannot be resolved (404)
+     * @throws InvalidConfigException When configuration is invalid
+     */
     public function resolve(HttpRequest $request)
     {
         /* @var $correctRule \Framework\Rule */
@@ -54,7 +81,7 @@ class Router
         // resolve route (swap <sth> with the value from request)
         $route = explode('/', $correctRule->route);
         if (count($route) != 2) {
-            throw new \Exception('Invalid route');
+            throw new InvalidConfigException('Invalid route');
         }
 
         $routeFilledWithParameters = [];
--- Framework/Rule.php
+++ Framework/Rule.php
@@ -2,6 +2,13 @@
 
 namespace Framework;
 
+use Framework\Exception\InvalidConfigException;
+
+/**
+ * Class Rule represents rule used for routing request.
+ *
+ * @package Framework
+ */
 class Rule
 {
     /**
@@ -19,12 +26,32 @@ class Rule
      */
     public $parameters = [];
 
+    /**
+     * @param string $pattern Pattern used for matching
+     * @param string $route controller/action combination
+     * @throws InvalidConfigException
+     */
     public function __construct($pattern, $route)
     {
         $this->_compileRegex($this->_extractAttributes($pattern));
         $this->route = $route;
     }
 
+    /**
+     * Extracts attributes from pattern and stores them as array.
+     * For example:
+     * <controller:\w+>/<action:\w+>/<id:\d+>
+     * is being converted to:
+     * [
+     *      'controller' => '\w+',
+     *      'action' => '\w+',
+     *      'id' => '\d+',
+     * ]
+     *
+     * @param $pattern
+     * @return array Of parameters [name => regex]
+     * @throws InvalidConfigException When pattern is invalid
+     */
     private function _extractAttributes($pattern)
     {
         $namedParameterRegex = '#^<([a-zA-Z0-9]+):(.+)>$#'; // <something:regex>
@@ -45,13 +72,26 @@ class Rule
                 continue;
             }
 
-            throw new \Exception('The rule\'s pattern is invalid.');
+            throw new InvalidConfigException('The rule\'s pattern is invalid.');
         }
         $this->parameters = $attributes;
 
         return $this->parameters;
     }
 
+    /**
+     * Compiles regex from array of parameters.
+     * For example:
+     * [
+     *      'controller' => '\w+',
+     *      'action' => '\w+',
+     *      'id' => '\d+',
+     * ]
+     * is being converted to:
+     * #^(?P<controller>\w+)/(?P<action>\w+)/(?P<id>\d+)$#si
+     *
+     * @param array $attributes The attributes array
+     */
     private function _compileRegex($attributes)
     {
         // groups thank's to Kadet1090's code

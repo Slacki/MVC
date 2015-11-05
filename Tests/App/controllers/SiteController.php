87ac0bdd3073e3751ecc93fea51027095aff7dec
Kacper Czochara <kacperczochara@gmail.com>
2015-11-05 12:09:51 +0100

    Moved App files to apropriate directory.

---
+++ Tests/App/controllers/SiteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\controllers;
+
+use Framework\Controller;
+
+class SiteController extends Controller
+{
+    public function actionIndex()
+    {
+        echo 'action index';
+    }
+}

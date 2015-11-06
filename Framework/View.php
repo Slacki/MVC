11a2aea40c1407637322b979a4175f7ed4a92468
Kacper Czochara <kacperczochara@gmail.com>
2015-11-06 20:31:40 +0100

    Data can now be passed to View files.

--- Framework/View.php
+++ Framework/View.php
@@ -4,7 +4,8 @@ namespace Framework;
 
 class View
 {
-    private $viewsDirectory;
+    private $_viewsDirectory;
+    private $_data = [];
 
     public function __construct()
     {
@@ -13,11 +14,16 @@ class View
 
     public function render($view, $useLayout = true)
     {
+        $viewPath = $this->_viewsDirectory . $view . '.php';
+        if (!file_exists($viewPath)) {
+            throw new \Exception('View ' . $view . 'does not exist.');
+        }
+
         ob_start();
         if ($useLayout) {
             $this->_renderWithLayout($view);
         } else {
-            require($this->viewsDirectory . $view . '.php');
+            require($this->_viewsDirectory . $view . '.php');
         }
         ob_end_flush();
     }
@@ -25,8 +31,20 @@ class View
     private function _renderWithLayout($view)
     {
         // primitive layout as header and footer files
-        require($this->viewsDirectory . 'header.php');
-        require($this->viewsDirectory . $view . '.php');
-        require($this->viewsDirectory . 'footer.php');
+        require($this->_viewsDirectory . 'header.php');
+        require($this->_viewsDirectory . $view . '.php');
+        require($this->_viewsDirectory . 'footer.php');
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->_data)) {
+            return $this->_data[$name];
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        $this->_data[$name] = $value;
     }
 }
